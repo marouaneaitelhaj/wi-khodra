e5dbfbb03c5d5ea4bce3fc77a6b373dd4a33a888
marwanee <marwaneaitelhaj001@gmail.com>
2025-02-17 23:47:43 +0100

    Add initial application setup with Maven configuration, application properties, and basic test structure

---
+++ farmers/src/test/java/com/wi/farmers/FarmersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wi.farmers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
